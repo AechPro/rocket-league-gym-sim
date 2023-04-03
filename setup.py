427e1264cbb8ee592acbce0ee293b93460686b27
Matthew Allen <AechPro@users.noreply.github.com>
2023-04-03 15:21:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 with open('README.md', 'r') as readme_file:
     long_description = readme_file.read()
