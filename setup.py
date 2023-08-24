f505cf96f0eda3db994f4dd2f9a8b57285f0bbc1
Alexx <eroszansan@gmail.com>
2023-08-24 12:10:11 +0100

    Version Bump

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-__version__ = "1.2.4"
+__version__ = "1.2.5"
 
-with open('README.md', 'r') as readme_file:
+with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
-    
+
 setup(
-    name='rlgym-sim',
+    name="rlgym-sim",
     packages=find_packages(),
     version=__version__,
-    description='A clone of RLGym for use with RocketSim in reinforcement learning projects.',
+    description="A clone of RLGym for use with RocketSim in reinforcement learning projects.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Lucas Emery, Matthew Allen, Zealan, and Mtheall',
-    url='https://github.com/AechPro/rocket-league-gym-sim',
+    long_description_content_type="text/markdown",
+    author="Lucas Emery, Matthew Allen, Zealan, and Mtheall",
+    url="https://github.com/AechPro/rocket-league-gym-sim",
     install_requires=[
-        'gym>=0.17',
-        'numpy>=1.19',
+        "gym>=0.17",
+        "numpy>=1.19",
     ],
-    python_requires='>=3.7',
-    license='Apache 2.0',
-    license_file='LICENSE',
-    keywords=['rocket-league', 'gym', 'reinforcement-learning', 'simulation']
+    python_requires=">=3.7",
+    license="Apache 2.0",
+    license_file="LICENSE",
+    keywords=["rocket-league", "gym", "reinforcement-learning", "simulation"],
 )
